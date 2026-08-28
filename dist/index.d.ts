5bdff3ac8b8cc736a7342da50cc7bd2afe353d1e
Nnamdi Michael Okpala <obinexusmk2@proton.me>
2026-08-28 20:26:12 +0100

    obix-adapter-oop

---
+++ dist/index.d.ts
@@ -0,0 +1,18 @@
+import type { DOPArtifact, State, Props, Payload, ActionTrace, ValidationResult } from "@obinexusltd/obix-spec";
+export interface OOPInstance<S extends object, P extends object> {
+    readonly state: S;
+    readonly props: P;
+    dispatch(actionName: string, payload?: Payload): S;
+    replay(trace: ActionTrace): S;
+    render(): string;
+    validate(): ValidationResult;
+    [action: string]: unknown;
+}
+export interface OOPConstructor<S extends object, P extends object> {
+    new (opts?: {
+        state?: S;
+        props?: Partial<P>;
+    }): OOPInstance<S, P>;
+}
+export declare function toOOP<S extends object = State, P extends object = Props>(artifact: DOPArtifact<S, P>): OOPConstructor<S, P>;
+//# sourceMappingURL=index.d.ts.map
---
+++ dist/index.js
@@ -0,0 +1,54 @@
+import { applyAction } from "@obinexusltd/obix-ir";
+export function toOOP(artifact) {
+    class Component {
+        #state;
+        #props;
+        constructor(opts = {}) {
+            this.#state = opts.state ?? artifact.initialState;
+            this.#props = Object.freeze({ ...artifact.props, ...(opts.props ?? {}) });
+        }
+        get state() {
+            return this.#state;
+        }
+        set state(_v) {
+            throw new TypeError(`[OBIX] ${artifact.name}.state is read-only`);
+        }
+        get props() {
+            return this.#props;
+        }
+        dispatch(actionName, payload) {
+            this.#state = applyAction(artifact, this.#state, actionName, payload, this.#props);
+            return this.#state;
+        }
+        replay(trace) {
+            for (const [name, payload] of trace)
+                this.dispatch(name, payload);
+            return this.#state;
+        }
+        render() {
+            return artifact.render ? artifact.render(this.#state, this.#props) : "";
+        }
+        validate() {
+            return artifact.validate ? artifact.validate(this.#state, this.#props) : { valid: true, violations: [] };
+        }
+    }
+    for (const name of Object.keys(artifact.actions)) {
+        Object.defineProperty(Component.prototype, name, {
+            value(payload) {
+                return this.dispatch(name, payload);
+            },
+            writable: false,
+            enumerable: false,
+        });
+    }
+    for (const name of Object.keys(artifact.derived)) {
+        Object.defineProperty(Component.prototype, name, {
+            get() {
+                return artifact.derived[name](this.state, this.props);
+            },
+            enumerable: false,
+        });
+    }
+    return Component;
+}
+//# sourceMappingURL=index.js.map
